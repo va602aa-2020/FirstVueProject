d4d5c220ae3e39198340486236038d12d2a9dea2
rinziv <rinzivillo@isti.cnr.it>
2020-04-17 17:32:07 +0200

    introducing slider input form

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
   <div id="app">
-
+    <b-container>
+      <b-row>
+        <b-col cols="4">
+          <h4>Slider</h4>
+          <div>
+            <label for="rangeNumber">Select a number</label>
+            <b-form-input id="rangeNumber" v-model="value"
+                          type="range" min="0" max="100"></b-form-input>
+            <div class="mt-2">Value: {{ value }}</div>
+          </div>
+        </b-col>
+        <b-col cols="8">D3.js visualization</b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
@@ -10,6 +23,11 @@ export default {
   name: 'App',
   components: {
 
+  },
+  data(){
+    return {
+      value: 10,
+    }
   }
 }
 </script>
