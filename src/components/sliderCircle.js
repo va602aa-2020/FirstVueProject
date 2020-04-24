7cd0a551c795107d7809680e0d37b3152847e618
rinziv <rinzivillo@isti.cnr.it>
2020-04-24 18:06:51 +0200

    introducing g container and data inheritance

--- src/components/sliderCircle.js
+++ src/components/sliderCircle.js
@@ -1,4 +1,4 @@
-const d3 =require('d3');
+const d3 = require('d3');
 
 export default function () {
     let width = 420;
@@ -9,19 +9,53 @@ export default function () {
         .range([0, 100]);
 
     const c = d3.scaleSequential(d3.interpolateYlOrRd)
-        .domain([0,5000])
-        ;
+        .domain([0, 5000])
+    ;
 
     function me(selection) {
         const myData = selection.datum();
-        selection.selectAll('circle')
+        // selection.selectAll('circle')
+        //     .data(myData)
+        //     .join('circle')
+        //     .attr('cx', width / 2)
+        //     .attr('cy', height / 2)
+        //     .attr('r', d => r(d))
+        //     .attr('fill', d => c(d));
+        //     // .attr('fill-opacity', 0.5);
+        //
+        // selection.selectAll('text')
+        //     .data(myData)
+        //     .join('text')
+        //     .attr('x', width/2)
+        //     .attr('y', height /2)
+        //     .attr('text-anchor', 'middle')
+        //     .attr('dy', 5)
+        //     .text(d => d);
+
+        const gCircle = selection.selectAll('g.circle')
             .data(myData)
+            .join('g')
+            .classed('circle', true);
+
+        gCircle
+            .selectAll('circle')
+            .data(d => [d])
             .join('circle')
             .attr('cx', width / 2)
             .attr('cy', height / 2)
             .attr('r', d => r(d))
-            .attr('fill', d => c(d));
-            // .attr('fill-opacity', 0.5);
+            .attr('fill', d => c(d))
+
+        gCircle
+            .selectAll('text')
+            .data(d => [d])
+            .join('text')
+            .attr('x', width / 2)
+            .attr('y', height / 2)
+            .attr('text-anchor', 'middle')
+            .attr('dy', 5)
+            .text(d => d);
+
     }
 
     me.width = function (value) {
